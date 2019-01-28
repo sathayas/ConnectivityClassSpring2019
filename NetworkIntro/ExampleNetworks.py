764353a8cb3f17d04a883da8a7cd401d53259452
Satoru Hayasaka <hayasaka@utexas.edu>
2019-01-28 11:25:58 -0600

    Updated the data directory; changed the color map for the football network; removed Florentine network

--- NetworkIntro/ExampleNetworks.py
+++ NetworkIntro/ExampleNetworks.py
@@ -2,7 +2,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 # loading the Les Miserables network
-G = nx.read_gml('lesmis.gml')
+G = nx.read_gml('IntroNetworkData/lesmis.gml')
 
 # drawing the graph  --- Kamada-Kawai layout
 plt.figure(figsize=[9,9])
@@ -16,7 +16,7 @@ plt.show()
 
 
 # loading the college football network
-G = nx.read_gml('football.gml')
+G = nx.read_gml('IntroNetworkData/football.gml')
 
 # drawing the graph  --- Kamada-Kawai layout
 plt.figure(figsize=[12,12])
@@ -28,7 +28,7 @@ for i,d in G.nodes(data=True):
 # drawing nodes, different conferences in different colors
 for iConf in range(12):
     nx.draw_networkx_nodes(G, pos,
-                           cmap=plt.cm.Vega20, node_color=conf)
+                           cmap=plt.cm.tab20b, node_color=conf)
 nx.draw_networkx_edges(G, pos, edge_color='lightblue')
 nx.draw_networkx_labels(G, pos, font_size=10, font_color='DarkGreen')
 plt.axis('off')
@@ -36,36 +36,9 @@ plt.title('College football network')
 plt.show()
 
 
-# loading the Florentine family network
-G = nx.read_pajek('Padgett.paj')
-
-# drawing the graph  --- Kamada-Kawai layout
-plt.figure(figsize=[8,8])
-pos = nx.kamada_kawai_layout(G, weight=None) # positions for all nodes
-nx.draw_networkx_nodes(G, pos)
-nx.draw_networkx_edges(G, pos, edge_color='lightblue')
-nx.draw_networkx_labels(G, pos, font_size=10, font_color='DarkGreen')
-plt.axis('off')
-plt.title('Florentine family network')
-plt.show()
-
-
-# loading the Chesapeake Bay food web
-G = nx.read_pajek('Chesapeake.paj')
-
-# drawing the graph  --- Kamada-Kawai layout
-plt.figure(figsize=[9,9])
-pos = nx.kamada_kawai_layout(G, weight=None) # positions for all nodes
-nx.draw_networkx_nodes(G, pos, node_color='Salmon', node_size=100)
-nx.draw_networkx_edges(G, pos, edge_color='lightblue')
-nx.draw_networkx_labels(G, pos, font_size=10, font_color='DarkGreen')
-plt.axis('off')
-plt.title('Chesapeake Bay food web')
-plt.show()
-
 
 # loading the C Elegans neural network
-G = nx.read_gml('celegansneural.gml')
+G = nx.read_gml('IntroNetworkData/celegansneural.gml')
 
 # drawing the graph  --- random
 plt.figure(figsize=[10,10])
@@ -75,4 +48,6 @@ nx.draw_networkx_edges(G, pos, edge_color='lightblue')
 plt.axis('off')
 plt.title('C Elegans neural network')
 plt.show()
+
+
 
