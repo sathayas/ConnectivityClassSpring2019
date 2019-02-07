35bd885ec525fe62f5655e009ff1d8213b76d2c2
Satoru Hayasaka <hayasaka@utexas.edu>
2019-02-07 16:10:10 -0600

    Added Eglob for voxel net, since it takes hours to calculate

--- NetworkStats/GlobalEfficiency.py
+++ NetworkStats/GlobalEfficiency.py
@@ -4,13 +4,13 @@ import numpy as np
 
 ##### loading the network data
 # C Elegans neural network
-G_CEleg = nx.read_adjlist('DataSmallWorld/CElegans.adjlist')
+G_CEleg = nx.read_adjlist('DataNetStats/CElegans.adjlist')
 # Power grid
-G_Power = nx.read_gml('DataSmallWorld/power.gml', label='id')
+G_Power = nx.read_gml('DataNetStats/power.gml', label='id')
 # Brain (ROI)
-G_ROI = nx.read_adjlist('DataSmallWorld/Oxford_sub16112_aal90_d5.adjlist')
+G_ROI = nx.read_adjlist('DataNetStats/Oxford_sub16112_aal90_d5.adjlist')
 # Brain (Voxel)
-G_Voxel = nx.read_adjlist('DataSmallWorld/Oxford_sub16112_voxel_d20.adjlist')
+G_Voxel = nx.read_adjlist('DataNetStats/Oxford_sub16112_voxel_d20.adjlist')
 
 ##### Global efficiencies
 print('Global eddiciencies')
@@ -18,3 +18,4 @@ print('C. Elegans: %5.3f' % nx.global_efficiency(G_CEleg))
 print('Power grid: %5.3f' % nx.global_efficiency(G_Power))
 print('Brain (ROI): %5.3f' % nx.global_efficiency(G_ROI))
 print('Brain (Voxel): %5.3f' % nx.global_efficiency(G_Voxel))
+#0.1869544358446755
