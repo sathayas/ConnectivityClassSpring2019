d9f19006c58b65388acc88b625d4bd009baf9de8
Satoru Hayasaka <hayasaka@utexas.edu>
2019-02-15 15:48:53 -0600

    Edited for the data set for this class

--- Centrality/DegreeCentral.py
+++ Centrality/DegreeCentral.py
@@ -1,63 +1,107 @@
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
-# loading network data
-G_karate = nx.read_gml('karate.gml', label='id')  # Karate network
-G_netsci = nx.read_gml('netscience.gml')  # network science co-authorship
+##### loading the network data
+# Florentine family network
+G_Flo = nx.Graph(nx.read_pajek('DataCentrality/Padgett.paj'))
+G_Flo.remove_node('Pucci') # removing isolated node
+# Brain (Berlin)
+G_Berlin = nx.read_adjlist('DataCentrality/Berlin_sub91116_aal90_d10_annotated.adjlist')
+# Brain (Leiden)
+G_Leiden = nx.read_adjlist('DataCentrality/Leiden_sub30943_aal90_d10_annotated.adjlist')
+# Brain (New York)
+G_NY = nx.read_adjlist('DataCentrality/NewYork_sub78118_aal90_d10_annotated.adjlist')
+# Brain (Oxford)
+G_Oxford = nx.read_adjlist('DataCentrality/Oxford_sub16112_aal90_d10_annotated.adjlist')
+# Brain (Queensland)
+G_Queen = nx.read_adjlist('DataCentrality/Queensland_sub42533_aal90_d10_annotated.adjlist')
 
-# degree centrality
-Cdeg_karate = nx.degree_centrality(G_karate)  
-Cdeg_netsci = nx.degree_centrality(G_netsci)  
 
 
 
-# sorting nodes by degree centrality
-# first, karate network
-Cdeg_karate_node = Cdeg_karate.keys()
-Cdeg_karate_k = Cdeg_karate.values()
-sortedNodes_karate = sorted(zip(Cdeg_karate_node, Cdeg_karate_k), 
+##### degree centrality
+Cdeg_Flo = nx.degree_centrality(G_Flo)
+Cdeg_Berlin = nx.degree_centrality(G_Berlin)  
+
+
+
+
+##### sorting nodes by degree centrality
+# first, Florentine network
+Cdeg_Flo_node = Cdeg_Flo.keys()
+Cdeg_Flo_k = Cdeg_Flo.values()
+sortedNodes_Flo = sorted(zip(Cdeg_Flo_node, Cdeg_Flo_k), 
                             key=lambda x: x[1], reverse=True)
-sCdeg_karate_node, sCdeg_karate_k = zip(*sortedNodes_karate)
+sCdeg_Flo_node, sCdeg_Flo_k = zip(*sortedNodes_Flo)
 
-# next, netsci network
-Cdeg_netsci_node = Cdeg_netsci.keys()
-Cdeg_netsci_k = Cdeg_netsci.values()
-sortedNodes_netsci = sorted(zip(Cdeg_netsci_node, Cdeg_netsci_k), 
+# next, brain network (Berlin)
+Cdeg_Berlin_node = Cdeg_Berlin.keys()
+Cdeg_Berlin_k = Cdeg_Berlin.values()
+sortedNodes_Berlin = sorted(zip(Cdeg_Berlin_node, Cdeg_Berlin_k), 
                             key=lambda x: x[1], reverse=True)
-sCdeg_netsci_node, sCdeg_netsci_k = zip(*sortedNodes_netsci)
+sCdeg_Berlin_node, sCdeg_Berlin_k = zip(*sortedNodes_Berlin)
 
 
 
-# top nodes and their degree centrality
-print('Karate network -- Top degree centrality nodes')
+###### top nodes and their degree centrality
+print('Florentine family network -- Top degree centrality nodes')
 print('Node           \tDegree centrality')
 for iNode in range(5):
-    print('%-14s\t' % str(sCdeg_karate_node[iNode]), end='')
-    print('%6.4f' % sCdeg_karate_k[iNode])
+    print('%-14s\t' % str(sCdeg_Flo_node[iNode]), end='')
+    print('%6.4f' % sCdeg_Flo_k[iNode])
 print()
 
-print('Network science co-authorship network -- Top degree centrality nodes')
-print('Node           \tDegree centrality')
-for iNode in range(15):
-    print('%-14s\t' % str(sCdeg_netsci_node[iNode]), end='')
-    print('%6.4f' % sCdeg_netsci_k[iNode])
+print('Brain network (Berlin) -- Top degree centrality nodes')
+print('Node           \t\tDegree centrality')
+for iNode in range(10):
+    print('%-20s\t' % str(sCdeg_Berlin_node[iNode]), end='')
+    print('%6.4f' % sCdeg_Berlin_k[iNode])
 print()
 
 
 
 
-# drawing the graph (karate network only) --- Kamada-Kawai layout
+###### drawing the graph (Florentine network) --- Kamada-Kawai layout
 plt.figure(figsize=[9,9])
-pos = nx.kamada_kawai_layout(G_karate, weight=None) # positions for all nodes
-nx.draw_networkx_nodes(G_karate, pos, 
-                       cmap=plt.cm.coolwarm, node_color=list(Cdeg_karate_k))
-nx.draw_networkx_edges(G_karate, pos, edge_color='lightblue')
-nx.draw_networkx_labels(G_karate, pos, font_size=10, font_color='White')
+pos = nx.kamada_kawai_layout(G_Flo, weight=None) # positions for all nodes
+nx.draw_networkx_nodes(G_Flo, pos, 
+                       cmap=plt.cm.coolwarm, node_color=list(Cdeg_Flo_k))
+nx.draw_networkx_edges(G_Flo, pos, edge_color='lightblue')
+nx.draw_networkx_labels(G_Flo, pos, font_size=10, font_color='darkorange')
 plt.axis('off')
-plt.title('Karate network\nand degree centrality')
-vmin = sCdeg_karate_k[-1]
-vmax = sCdeg_karate_k[0]
+plt.title('Florentine family network\nand degree centrality')
+vmin = sCdeg_Flo_k[-1]
+vmax = sCdeg_Flo_k[0]
+sm = plt.cm.ScalarMappable(cmap=plt.cm.coolwarm, 
+                           norm=plt.Normalize(vmin=vmin, vmax=vmax))
+sm._A = []
+cbar = plt.colorbar(sm, shrink=0.5)
+cbar.ax.set_ylabel('Degree centrality')
+plt.show()
+
+
+
+###### drawing the graph (Brain, Berlin) --- their brain space coordinate
+# loading the coordinates info for brain areas
+AALTable = pd.read_csv('DataCentrality/aal_MNI_V4_coord.csv')
+# dictionary of xy-coordinates
+pos = {}
+for i in range(1,91):
+    pos[AALTable.iloc[i-1,1]] = np.array(AALTable.loc[i-1,
+                                                      ['centerX',
+                                                       'centerY']])
+
+plt.figure(figsize=[9,9])
+nx.draw_networkx_nodes(G_Berlin, pos, 
+                       cmap=plt.cm.coolwarm, node_color=list(Cdeg_Berlin_k))
+nx.draw_networkx_edges(G_Berlin, pos, edge_color='lightblue')
+nx.draw_networkx_labels(G_Berlin, pos, font_size=7, font_color='black')
+plt.axis('off')
+plt.title('Brain network (Berlin)\nand degree centrality')
+vmin = sCdeg_Berlin_k[-1]
+vmax = sCdeg_Berlin_k[0]
 sm = plt.cm.ScalarMappable(cmap=plt.cm.coolwarm, 
                            norm=plt.Normalize(vmin=vmin, vmax=vmax))
 sm._A = []
