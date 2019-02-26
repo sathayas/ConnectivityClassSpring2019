d8be7e3f978d9ca0ae1cad2ee85050aec21927a4
Satoru Hayasaka <hayasaka@utexas.edu>
2019-02-26 12:07:01 -0600

    Girvan-Newman, and Louvain are the two main community detection algorithms

--- Modules/CommunityExamples.py
+++ Modules/CommunityExamples.py
@@ -1,11 +1,17 @@
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
-from networkx.algorithms.community import label_propagation_communities
 from networkx.algorithms.community import girvan_newman, modularity
+import community   # Louvain method
+
+##### Custom distinct color function --- to be used later
+def get_cmap(n, name='hsv'):
+    '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct 
+    RGB color; the keyword argument name must be a standard mpl colormap name.'''
+    return plt.cm.get_cmap(name, n)
 
 
-# girman-newman method, optimized with modularity
+##### girman-newman method, optimized with modularity
 def girvan_newman_opt(G, verbose=False):
     runningMaxMod = 0
     commIndSetFull = girvan_newman(G)
@@ -21,26 +27,35 @@ def girvan_newman_opt(G, verbose=False):
 
 
 
-# loading network data
-G_karate = nx.read_gml('karate.gml', label='id')  # Karate network
-G_football = nx.read_gml('football.gml')  # Football network
+##### loading network data
+# Karate network
+G_karate = nx.read_gml('DataModules/karate.gml', label='id')  
+# Football network
+G_football = nx.read_gml('DataModules/football.gml')  
 
 
-# Community detection with the label propagation algorithm
-commIndSet_karate = label_propagation_communities(G_karate)
-commInd_karate_lp = [list(x) for x in iter(commIndSet_karate)]
-
-commIndSet_football = label_propagation_communities(G_football)
-commInd_football_lp = [list(x) for x in iter(commIndSet_football)]
-
-
+##### Community detection 
 # Community detection with the girvan-newman algorithm
-commInd_karate_gn = girvan_newman_opt(G_karate)
-commInd_football_gn = girvan_newman_opt(G_football)
+commInd_karate = girvan_newman_opt(G_karate)
+commInd_football = girvan_newman_opt(G_football)
+# converting the partitions into dictionaries
+partition_karate_GN = {}
+for i,iComm in enumerate(commInd_karate):
+    for iNode in iComm:
+        partition_karate_GN[iNode] = i
+partition_football_GN = {}
+for i,iComm in enumerate(commInd_football):
+    for iNode in iComm:
+        partition_football_GN[iNode] = i
+
+
+# Community detection with the Louvain method
+partition_karate_L = community.best_partition(G_karate)
+partition_football_L = community.best_partition(G_football)
 
 
 
-# drawing the graph (karate network)
+###### drawing the graph (karate network)
 plt.figure(figsize=[9,4])
 
 # first, graph without community assignments
@@ -55,34 +70,40 @@ plt.axis('off')
 plt.xlim([-0.6, 0.65])
 plt.ylim([-0.85, 1.2])
 
-# next, graph with communities in different colors (label propagation)
+# next, graph with communities in different colors (Girvan-Newman)
 plt.subplot(132)
-for iComm in range(len(commInd_karate_lp)):
-    nx.draw_networkx_nodes(G_karate, pos, nodelist=commInd_karate_lp[iComm],
-                           cmap=plt.cm.rainbow,
-                           vmin=0, vmax=len(commInd_karate_lp)-1,
-                           node_color = [iComm]*len(commInd_karate_lp[iComm]),
+nComm = max([comm for comm in partition_karate_GN.values()])+1
+node_color_list = get_cmap(nComm+1,'rainbow')
+for iComm in range(nComm):
+    nodeList = [iNode for iNode,Comm in partition_karate_GN.items()
+                if Comm==iComm]
+    nx.draw_networkx_nodes(G_karate, pos, 
+                           nodelist=nodeList,
+                           node_color = node_color_list(iComm),
                            node_size=300)
 nx.draw_networkx_edges(G_karate, pos,
                        edge_color='lightblue')
 nx.draw_networkx_labels(G_karate, pos, font_size=10, font_color='White')
-plt.title('Label propagation')
+plt.title('Girvan-Newman method')
 plt.axis('off')
 plt.xlim([-0.6, 0.65])
 plt.ylim([-0.85, 1.2])
 
-# (girvan-newman)
+# finally, graph with communities in different colors (Louvain)
 plt.subplot(133)
-for iComm in range(len(commInd_karate_gn)):
-    nx.draw_networkx_nodes(G_karate, pos, nodelist=commInd_karate_gn[iComm],
-                           cmap=plt.cm.rainbow,
-                           vmin=0, vmax=len(commInd_karate_gn)-1,
-                           node_color = [iComm]*len(commInd_karate_gn[iComm]),
+nComm = max([comm for comm in partition_karate_L.values()])+1
+node_color_list = get_cmap(nComm+1,'rainbow')
+for iComm in range(nComm):
+    nodeList = [iNode for iNode,Comm in partition_karate_L.items()
+                if Comm==iComm]
+    nx.draw_networkx_nodes(G_karate, pos, 
+                           nodelist=nodeList,
+                           node_color = node_color_list(iComm),
                            node_size=300)
 nx.draw_networkx_edges(G_karate, pos,
                        edge_color='lightblue')
 nx.draw_networkx_labels(G_karate, pos, font_size=10, font_color='White')
-plt.title('Girvan-Newman')
+plt.title('Louvain method')
 plt.axis('off')
 plt.xlim([-0.6, 0.65])
 plt.ylim([-0.85, 1.2])
@@ -93,7 +114,7 @@ plt.show()
 
 
 
-# drawing the graph (football network)
+#### drawing the graph (football network)
 plt.figure(figsize=[10,4.5])
 plt.subplot(131)
 
@@ -108,34 +129,40 @@ plt.axis('off')
 plt.xlim([-1.15, 1.15])
 plt.ylim([-1.15, 1.15])
 
-# next, graph with communities in different colors (label propagation)
+# next, graph with communities in different colors (Girvan-Newman)
 plt.subplot(132)
-for iComm in range(len(commInd_football_lp)):
-    nx.draw_networkx_nodes(G_football, pos, nodelist=commInd_football_lp[iComm],
-                           cmap=plt.cm.rainbow,
-                           vmin=0, vmax=len(commInd_football_lp)-1,
-                           node_color = [iComm]*len(commInd_football_lp[iComm]),
+nComm = max([comm for comm in partition_football_GN.values()])+1
+node_color_list = get_cmap(nComm+1,'rainbow')
+for iComm in range(nComm):
+    nodeList = [iNode for iNode,Comm in partition_football_GN.items()
+                if Comm==iComm]
+    nx.draw_networkx_nodes(G_football, pos, 
+                           nodelist=nodeList,
+                           node_color = node_color_list(iComm),
                            node_size=100)
 nx.draw_networkx_edges(G_football, pos,
                        edge_color='lightblue')
-nx.draw_networkx_labels(G_football, pos, font_size=7, font_color='Black')
-plt.title('Label propagation')
+nx.draw_networkx_labels(G_football, pos, font_size=7, font_color='black')
+plt.title('Girvan-Newman method')
 plt.axis('off')
 plt.xlim([-1.15, 1.15])
 plt.ylim([-1.15, 1.15])
 
-# (girvan-newman)
+# next, graph with communities in different colors (Louvain)
 plt.subplot(133)
-for iComm in range(len(commInd_football_gn)):
-    nx.draw_networkx_nodes(G_football, pos, nodelist=commInd_football_gn[iComm],
-                           cmap=plt.cm.rainbow,
-                           vmin=0, vmax=len(commInd_football_gn)-1,
-                           node_color = [iComm]*len(commInd_football_gn[iComm]),
+nComm = max([comm for comm in partition_football_L.values()])+1
+node_color_list = get_cmap(nComm+1,'rainbow')
+for iComm in range(nComm):
+    nodeList = [iNode for iNode,Comm in partition_football_L.items()
+                if Comm==iComm]
+    nx.draw_networkx_nodes(G_football, pos, 
+                           nodelist=nodeList,
+                           node_color = node_color_list(iComm),
                            node_size=100)
 nx.draw_networkx_edges(G_football, pos,
                        edge_color='lightblue')
-nx.draw_networkx_labels(G_football, pos, font_size=7, font_color='Black')
-plt.title('Girvan-Newman')
+nx.draw_networkx_labels(G_football, pos, font_size=7, font_color='black')
+plt.title('Louvain method')
 plt.axis('off')
 plt.xlim([-1.15, 1.15])
 plt.ylim([-1.15, 1.15])
