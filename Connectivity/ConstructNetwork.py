ffaacdabf31bd42761207d25fbc502f1dbaf3896
Satoru Hayasaka <hayasaka@utexas.edu>
2019-03-22 11:08:10 -0500

    now uses an external function to calculate mutual info

--- Connectivity/ConstructNetwork.py
+++ Connectivity/ConstructNetwork.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 # importing external functions for MI and PC calculation
 from partial_corr import partial_corr
-
+from mutual_info import mutual_information
 
 ###### Parameters
 targetDeg = 10  # target average degree
@@ -15,42 +15,8 @@ nBins = 11  # number of bins for mutual information
 
 
 
-###### Mutual information function
-def mutual_information(X, bins):
-    A = np.zeros([X.shape[1], X.shape[1]])
-    for i in range(X.shape[1]):
-        print('Working on row i: %d' % i)
-        for j in range(i+1, X.shape[1]):
-            x1 = X[:,i]
-            x2 = X[:,j]
-            A[i,j] = calc_MI(x1,x2,bins)
-            A[j,i] = A[i,j]
-    return A
-
-
-def calc_MI(X,Y,bins):
-
-   c_XY = np.histogram2d(X,Y,bins)[0]
-   c_X = np.histogram(X,bins)[0]
-   c_Y = np.histogram(Y,bins)[0]
-
-   H_X = shan_entropy(c_X)
-   H_Y = shan_entropy(c_Y)
-   H_XY = shan_entropy(c_XY)
-
-   MI = H_X + H_Y - H_XY
-   return MI
-
-def shan_entropy(c):
-    c_normalized = c / float(np.sum(c))
-    c_normalized = c_normalized[np.nonzero(c_normalized)]
-    H = -sum(c_normalized* np.log2(c_normalized))  
-    return H
-
 
 ###### Network construction functions, based on correlation matrix
-
-
 
 def net_builder_HardTh(R, NodeInd, K, cType=1):
     '''
