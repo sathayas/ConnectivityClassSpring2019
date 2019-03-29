4dba15d7a88cabf43035f22eaab7fef389fc740b
Satoru Hayasaka <hayasaka@utexas.edu>
2019-03-29 11:39:29 -0500

    Complete overhaul. Now regresses out finger, foot, and lips time series

--- TaskNetwork/fMRIPreProp/preproc_fingerfootlips.py
+++ TaskNetwork/fMRIPreProp/preproc_fingerfootlips.py
@@ -13,90 +13,52 @@ BaseDir = '/home/satoru/Projects/ModularMapping/Analysis/Pilot_R21_Oct2017/ds114
 fStruct = os.path.join(BaseDir, 'sub-03_ses-test_T1w.nii.gz')
 fStruct_bet = os.path.join(BaseDir, 'sub-03_ses-test_T1w_brain.nii.gz')
 ffMRI_orig = os.path.join(BaseDir, 'sub-03_ses-test_task-fingerfootlips_bold.nii.gz')
-
 ListTiming = [os.path.join(BaseDir,'onset-finger-minus10s.txt'),
               os.path.join(BaseDir,'onset-foot-minus10s.txt'),
               os.path.join(BaseDir,'onset-lips-minus10s.txt')]
 
+
 # parameters
 imgDim = [46, 56, 42]
 voxSize = [4, 4, 4]
-tasks = ['finger', 'foot', 'lips', 'combo']
-models = ['NoRegress', 'Regress']
 
 
-# running bet
-MyCodes.run_bet(fStruct)
+# first, making a copy of the fMRI data
+ffMRI = os.path.join(BaseDir, 'PSY381D_fMRI_fingerfootlips.nii.gz')
+com_cp = 'cp ' + ffMRI_orig + ' ' + ffMRI
+res = os.system(com_cp)
 
+# directory business
+FeatDir = os.path.join(BaseDir, 'PSY381D_fMRI_fingerfootlips.feat')
+fGMMask = os.path.join(FeatDir, 'reg/highres2standard_seg_1_d_r.nii.gz')
 
-# running separate preprocessing for fMRI with or without GLM regression.
-for iModel in models:
+# running feat with normalization
+tmpDir = MyCodes.run_feat(fStruct_bet, ffMRI, bNorm=True, nVolDel=4)
 
-    # first, making a copy of the fMRI data
-    ffMRI = os.path.join(BaseDir, 'fMRI_' + iModel + '.nii.gz')
-    com_cp = 'cp ' + ffMRI_orig + ' ' + ffMRI
-    res = os.system(com_cp)
+# constructing the desing files for a block model
+GLM = MyCodes.run_feat_model(fStruct_bet, ffMRI, ListTiming, nVolDel=4)
 
-    # directory business
-    FeatDir = os.path.join(BaseDir, 'fMRI_' + iModel + '.feat')
-    fGMMask = os.path.join(FeatDir, 'reg/highres2standard_seg_1_d_r.nii.gz')
+# warping (or re-orienting, centering, and reslicing) the functional
+MyCodes.run_warp(FeatDir, imgDim, voxSize)
 
-    # running feat without normalization
-    tmpDir = MyCodes.run_feat(fStruct_bet, ffMRI, bNorm=False, nVolDel=4)
+# band-pass filtering
+MyCodes.run_bp(FeatDir)
 
-    # constructing the desing files for a block model
-    GLM = MyCodes.run_feat_model(fStruct_bet, ffMRI, ListTiming, nVolDel=4)
+# segmentation
+MyCodes.run_fast(FeatDir)
 
-    # warping (or re-orienting, centering, and reslicing) the functional
-    MyCodes.run_warp(FeatDir, imgDim, voxSize)
+# creating masks
+MyCodes.mask_gm(FeatDir)
+MyCodes.generate_mask(FeatDir, fGMMask)
 
-    # band-pass filtering
-    MyCodes.run_bp(FeatDir)
+# extracting the mean time series
+MyCodes.extract_global(FeatDir)
 
-    # segmentation
-    MyCodes.run_fast(FeatDir)
+# running regression
+MyCodes.regress_global(FeatDir, GLM)
 
-    # creating masks
-    MyCodes.mask_gm(FeatDir)
-    MyCodes.generate_mask(FeatDir, fGMMask)
+# motion scrubbing - disabled
+#MyCodes.scrub_motion(FeatDir)
 
-    # extracting the mean time series
-    MyCodes.extract_global(FeatDir)
-
-    # running regression
-    if iModel=='NoRegress':
-        MyCodes.regress_global(FeatDir)
-    else:
-        MyCodes.regress_global(FeatDir, GLM)
-
-    # motion scrubbing
-    MyCodes.scrub_motion(FeatDir)
-
-    
-    # the loop for different tasks
-    for iTask in tasks:
-
-        # directory business
-        CorrDir = os.path.join(FeatDir, 'CorrDir_'+iTask)
-        if not os.path.exists(CorrDir):
-            os.makedirs(CorrDir)
-
-        # GLM time series
-        if iTask=='finger':
-            ts = MyCodes.GLM_column(FeatDir,0)
-        elif iTask=='foot':
-            ts = MyCodes.GLM_column(FeatDir,2)
-        elif iTask=='lips':
-            ts = MyCodes.GLM_column(FeatDir,4)
-        else:
-            ts1 = MyCodes.GLM_column(FeatDir,0)
-            ts2 = MyCodes.GLM_column(FeatDir,2)
-            ts3 = MyCodes.GLM_column(FeatDir,4)
-            ts = ts1 + ts2 + ts3
-
-        # calculating the correlation matrix
-        MyCodes.run_crosscorr(CorrDir,PosOnly=1,ts=ts)
-
-            
 
 
