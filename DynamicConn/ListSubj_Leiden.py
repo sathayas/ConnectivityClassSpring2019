2e8627f1b70b5dec97f21a102ece9ccdba6ca78a
Satoru Hayasaka <hayasaka@utexas.edu>
2019-04-04 22:47:39 -0500

    Output is included in the code

--- DynamicConn/ListSubj_Leiden.py
+++ DynamicConn/ListSubj_Leiden.py
@@ -20,3 +20,25 @@ for iSubj in listSubj:
 print('%-12s' % 'Subject' + '%-14s' % 'N time points')
 for i,iSubj in enumerate(listSubj):
     print('%-12s' % iSubj + '%4d' % nTimePoints[i])
+
+
+#Subject     N time points 
+#sub64642     212
+#sub40907     212
+#sub18456     212
+#sub39335     212
+#sub72247     212
+#sub13537     204
+#sub66131     212
+#sub01787     212
+#sub97690     200
+#sub30943     212
+#sub86034     212
+#sub36743     212
+#sub92061     212
+#sub68050     212
+#sub57187     212
+#sub93194     212
+#sub04484     212
+#sub87320     212
+#sub52922     212
