ff1b8c4636822ece42f6cdfee78e767af5c6d497
Satoru Hayasaka <hayasaka@utexas.edu>
2019-04-11 21:52:41 -0500

    Now all files are included

--- fMRIPreprop/Preparation/CopyFiles.py
+++ fMRIPreprop/Preparation/CopyFiles.py
@@ -11,7 +11,22 @@ def copy_files(Site, SiteOut, subjID, outDir='.'):
     ffMRIout = os.path.join(outDir, SiteOut+'_'+subjID+'.nii.gz')
     copyfile(ffMRI, ffMRIout)
 
+    # mask image
+    fMask = os.path.join(dirOrigin, 'Processed.feat/reg/aal_fmri_brain.nii.gz')
+    fMaskout = os.path.join(outDir, SiteOut+'_'+subjID+'_mask.nii.gz')
+    copyfile(fMask, fMaskout)
 
+    # Phys par file (aka global means)
+    fPhysPar = os.path.join(dirOrigin, 'Processed.feat/reg/PhysPar.npz')
+    fPhysParout = os.path.join(outDir, SiteOut+'_'+subjID+'_PhysPar.npz')
+    copyfile(fPhysPar, fPhysParout)
+
+    # Motion parameter file 
+    fMoPar = os.path.join(dirOrigin, 'Processed.feat/mc/prefiltered_func_data_mcf.par')
+    fMoParout = os.path.join(outDir, SiteOut+'_'+subjID+'_MoPar.par')
+    copyfile(fMoPar, fMoParout)
+    
+    
 Site = 'Oxford'
 SiteOut = 'Oxford'
 subjID = 'sub16112'
